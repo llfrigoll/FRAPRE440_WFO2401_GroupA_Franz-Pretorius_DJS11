130b3f55110d9c0026755f7b7ce8290266401037
Franz <franzj.pretorius.2003@gmail.com>
2024-06-15 15:02:04 +0200

    Some more changes

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,54 +1,46 @@
-import React, { useEffect, useState } from "react"
-import Select, { StylesConfig, createFilter } from 'react-select'
-import { getAllPreviews, getGenres } from "../utils/Api"
-import LoadIcon from "./LoadIcon"
-import { Genre, Preview } from "../utils/interfaces"
+import React, { useEffect, useState } from "react";
+import Select, { StylesConfig } from 'react-select';
+import { getAllPreviews, getGenres } from "../utils/Api";
+import LoadIcon from "./LoadIcon";
+import { Genre, Preview } from "../utils/interfaces";
 
-
-
-export default function Filters({setState}: any) {
-    const [genreNames, setGenreNames] = useState<Object[]>([])
-    const [loading, setLoading] = useState(false)
-    const [genres, setGenres] = useState<Genre[]>([])
-    const [isGenreSelected, setIsGenreSelected] = useState(false)
-    const [selectedGenre, setSelectedGenre] = useState<any>(null)
-    const [previews, setPreviews] = useState<Preview[]>([])
-    const [defaultFilter, setDefaultFilter] = useState<Preview[]>([])
+export default function Filters({ setState }: any) {
+    const [genreNames, setGenreNames] = useState<Object[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [genres, setGenres] = useState<Genre[]>([]);
+    const [previews, setPreviews] = useState<Preview[]>([]);
+    const [defaultFilter, setDefaultFilter] = useState<Preview[]>([]);
 
     useEffect(() => {
         async function loadPreviews() {
-            const localPreviews = await getAllPreviews()
-            setPreviews(localPreviews)
-            const localDefaultFilter = [...previews]
-            setDefaultFilter(localDefaultFilter)
+            setLoading(true);
+            const localPreviews = await getAllPreviews();
+            setPreviews(localPreviews);
+            setDefaultFilter(localPreviews);
+            setLoading(false);
         }
-        loadPreviews()
-    },[])
-        
+        loadPreviews();
+    }, []);
+
     useEffect(() => {
         async function loadGenreFilter() {
-            setLoading(true)
-            const localGenres = await getGenres()
-            setGenres(localGenres)
-            const options = localGenres.map(genre => {
-                const option = {
-                    value: genre.title.toLowerCase(),
-                    label: genre.title
-                }
-                return option
-            })
-            setGenreNames(options)
-            setLoading(false)
+            const localGenres = await getGenres();
+            setGenres(localGenres);
+            const options = localGenres.map(genre => ({
+                value: genre.title.toLowerCase(),
+                label: genre.title
+            }));
+            setGenreNames(options);
         }
-        loadGenreFilter()
-    }, [])
-    
+        loadGenreFilter();
+    }, []);
+
     if (loading) {
         return (
             <div data-ref="dashboard-container" className="pt-20">
                 <LoadIcon />
             </div>
-        )
+        );
     }
 
     const customStyles: StylesConfig = {
@@ -65,123 +57,68 @@ export default function Filters({setState}: any) {
             color: "rgb(30,41,59)",
             fontWeight: 500
         })
-    }
+    };
 
     const handleGenreChange = (selectedOption: any) => {
-        if(!selectedOption) {
-            setSelectedGenre(null)
-            setState(previews)
+        if (!selectedOption) {
+            setState(previews);
         } else {
-            setSelectedGenre(selectedOption)
-            const selectedGenre = genres.find(genre => genre.title.toLowerCase() === selectedOption.value)
-            if(selectedGenre) {
-                let showIds = []
-                showIds = [...selectedGenre.shows]
-                const genreFilter = previews.filter(preview => showIds.includes(preview.id))
-                setState(genreFilter)
+            const selectedGenre = genres.find(genre => genre.title.toLowerCase() === selectedOption.value);
+            if (selectedGenre) {
+                const showIds = selectedGenre.shows;
+                const genreFilter = previews.filter(preview => showIds.includes(preview.id));
+                setState(genreFilter);
             }
         }
-    }
+    };
 
-    if(selectedGenre) {
-        setIsGenreSelected(true)
-    }else{
-        setIsGenreSelected(false)
-    }
+    const handleSort = (sortFunction: (a: Preview, b: Preview) => number) => {
+        const sortedPreviews = [...previews].sort(sortFunction);
+        setState(sortedPreviews);
+    };
 
     return (
-      <div
-        data-ref="filters-container"
-        className="flex justify-between rounded-sm bg-white w-2/5 ml-auto mr-auto mt-10 px-6 py-2"
-      >
-        <button
-          onClick={() => {
-            setPreviews([])
-            setPreviews([...defaultFilter])
-            if(isGenreSelected) {
-                handleGenreChange(selectedGenre)
-            }
-            setState(previews)}
-          }
-          className="hover:text-gray-500 hover:font-normal text-slate-800 font-medium"
+        <div
+            data-ref="filters-container"
+            className="flex justify-between rounded-sm bg-white w-2/5 ml-auto mr-auto mt-10 px-6 py-2"
         >
-          All
-        </button>
-        <button
-          onClick={() => {
-            // previews = []
-            // previews = [...defaultFilter]
-            const sortedPreviews = [...previews.sort((a: Preview, b: Preview) =>
-                a.title.localeCompare(b.title)
-              )]
-            setPreviews([...sortedPreviews])
-            if(isGenreSelected) {
-                handleGenreChange(selectedGenre)
-            }
-            setState(previews)}
-          }
-          className="hover:text-gray-500 hover:font-normal text-slate-800 font-medium"
-        >
-          A-Z
-        </button>
-        <button
-          onClick={() => {
-            // previews = []
-            // previews = [...defaultFilter]
-            const sortedPreviews = [...previews.sort((a: Preview, b: Preview) =>
-                -a.title.localeCompare(b.title)
-              )]
-              setPreviews([...sortedPreviews])
-            if(isGenreSelected) {
-                handleGenreChange(selectedGenre)
-            }
-            setState(previews)}
-          }
-          className="hover:text-gray-500 hover:font-normal text-slate-800 font-medium"
-        >
-          Z-A
-        </button>
-        <button
-          onClick={() => {
-            // previews = []
-            // previews = [...defaultFilter]
-            const sortedPreviews = [...previews.sort((a: Preview, b: Preview) => {
-                return -(new Date(a.updated).getTime() - new Date(b.updated).getTime());
-              })]
-              setPreviews([...sortedPreviews])
-              if(isGenreSelected) {
-                handleGenreChange(selectedGenre)
-            }
-            setState(previews)}
-          }
-          className="hover:text-gray-500 hover:font-normal text-slate-800 font-medium"
-        >
-          Newest
-        </button>
-        <button
-          onClick={() => {
-            // previews = []
-            // previews = [...defaultFilter]
-            const sortedPreviews = [...previews.sort((a: Preview, b: Preview) => {
-                return new Date(a.updated).getTime() - new Date(b.updated).getTime();
-              })]
-              setPreviews([...sortedPreviews])
-              if(isGenreSelected) {
-                handleGenreChange(selectedGenre)
-            }
-            setState(previews)}
-          }
-          className="hover:text-gray-500 hover:font-normal text-slate-800 font-medium"
-        >
-          Oldest
-        </button>
-        <Select
-          styles={customStyles}
-          options={genreNames}
-          isClearable={true}
-          placeholder="Genres"
-          onChange={handleGenreChange}
-        />
-      </div>
+            <button
+                onClick={() => setState(defaultFilter)}
+                className="hover:text-gray-500 hover:font-normal text-slate-800 font-medium"
+            >
+                All
+            </button>
+            <button
+                onClick={() => handleSort((a, b) => a.title.localeCompare(b.title))}
+                className="hover:text-gray-500 hover:font-normal text-slate-800 font-medium"
+            >
+                A-Z
+            </button>
+            <button
+                onClick={() => handleSort((a, b) => -a.title.localeCompare(b.title))}
+                className="hover:text-gray-500 hover:font-normal text-slate-800 font-medium"
+            >
+                Z-A
+            </button>
+            <button
+                onClick={() => handleSort((a, b) => new Date(b.updated).getTime() - new Date(a.updated).getTime())}
+                className="hover:text-gray-500 hover:font-normal text-slate-800 font-medium"
+            >
+                Newest
+            </button>
+            <button
+                onClick={() => handleSort((a, b) => new Date(a.updated).getTime() - new Date(b.updated).getTime())}
+                className="hover:text-gray-500 hover:font-normal text-slate-800 font-medium"
+            >
+                Oldest
+            </button>
+            <Select
+                styles={customStyles}
+                options={genreNames}
+                isClearable={true}
+                placeholder="Genres"
+                onChange={handleGenreChange}
+            />
+        </div>
     );
 }
