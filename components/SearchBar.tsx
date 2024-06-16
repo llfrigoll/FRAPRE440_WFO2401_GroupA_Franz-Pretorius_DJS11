afa2247822e478f95bdfe1c3d2b82de744ab68df
Franz <franzj.pretorius.2003@gmail.com>
2024-06-16 16:38:14 +0200

    Added onchange to searchbar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,15 +3,15 @@ import { Preview } from "../utils/interfaces";
 
 interface SearchBarProps {
     setState: (previews: Preview[]) => void;
-    previews: Preview[]
+    state: Preview[]
 }
 
-export default function SearchBar({ setState, previews }: SearchBarProps) {
+export default function SearchBar({ setState, state }: SearchBarProps) {
 
     function handleChange() {
         let textInput = document.querySelector('input[data-ref="search-input"]')?.textContent
         if(textInput) {
-            const filterPreviews = previews.filter(preview => {
+            const filterPreviews = state.filter(preview => {
                 return preview.title.toLowerCase().includes(textInput.toLowerCase())
             })
             setState(filterPreviews)
@@ -22,6 +22,5 @@ export default function SearchBar({ setState, previews }: SearchBarProps) {
         <div data-ref="search-bar" className="relative h-1/2 ml-auto mr-14 pt-5 mb-auto z-10 border-1 border-solid border-red-500">
             <input onChange={handleChange} type="text" data-ref="search-input" placeholder="Search" className="absolute p-2 pr-10 right-0 bg-slate-600 placeholder-slate-300 rounded-2xl text-slate-300 focus:outline focus:outline-2 focus:outline-slate-300"/>
         </div> 
-    )
-    
+    )  
 }
